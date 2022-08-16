87440f29aa8e498ec8fd5776c51c4825ef82dbba
Nicholas <njriasanovsky@berkeley.edu>
2022-08-16 17:55:44 -0400

    Added a new constructor for multiple default schema

--- core/src/main/java/org/apache/calcite/prepare/CalciteCatalogReader.java
+++ core/src/main/java/org/apache/calcite/prepare/CalciteCatalogReader.java
@@ -97,6 +97,29 @@ public class CalciteCatalogReader implements Prepare.CatalogReader {
         typeFactory, config);
   }
 
+
+  public CalciteCatalogReader(CalciteSchema rootSchema,
+      List<List<String>> defaultSchemas, RelDataTypeFactory typeFactory,
+      CalciteConnectionConfig config) {
+    this(rootSchema, SqlNameMatchers.withCaseSensitive(config != null && config.caseSensitive()),
+        validateDefaultSchemaPaths(defaultSchemas), typeFactory, config);
+  }
+
+  /**
+   * Validate that each default schema provided is not null.
+   * @param defaultSchemas List of provided schemas to serve as defaults.
+   * @return List of default schemas with an empty list appended as a sentinel.
+   */
+  private static List<List<String>> validateDefaultSchemaPaths(List<List<String>> defaultSchemas) {
+    List<List<String>> defaultSchemaPaths = new ArrayList<>();
+    for (List<String> defaultSchema: defaultSchemas) {
+      defaultSchemaPaths.add(
+          Objects.requireNonNull(defaultSchema, "defaultSchema"));
+    }
+    defaultSchemaPaths.add(ImmutableList.of());
+    return defaultSchemaPaths;
+  }
+
   protected CalciteCatalogReader(CalciteSchema rootSchema,
       SqlNameMatcher nameMatcher, List<List<String>> schemaPaths,
       RelDataTypeFactory typeFactory, CalciteConnectionConfig config) {
