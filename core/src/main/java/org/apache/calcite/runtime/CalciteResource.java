16442292caa9299eb7582b0108fb439f28277aed
Keaton T. Drebes <keaton@bodo.ai>
2023-03-06 10:56:37 -0500

    minor cleanupv2

--- core/src/main/java/org/apache/calcite/runtime/CalciteResource.java
+++ core/src/main/java/org/apache/calcite/runtime/CalciteResource.java
@@ -394,8 +394,7 @@ public interface CalciteResource {
 
   @BaseMessage("HAVING clause must be a condition")
   ExInst<SqlValidatorException> havingMustBeBoolean();
-
-
+  
   @BaseMessage("QUALIFY clause must be a boolean condition")
   ExInst<SqlValidatorException> qualifyMustBeBoolean();
 
